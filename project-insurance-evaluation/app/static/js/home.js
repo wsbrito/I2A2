2728d190edf4efd16ae8d62134907807a36b91fa
wsbrito <wsbrito1975@gmail.com>
2020-11-12 19:39:29 -0300

    [Insurance-Evaluation] Versão com a aplicação concluída. Faltanto retoques no css. Mudanças a serem submetidas: 	new file:   project-insurance-evaluation/app/static/images/capa.png 	new file:   project-insurance-evaluation/app/static/images/favicon.png 	modified:   project-insurance-evaluation/app/static/js/home.js 	new file:   project-insurance-evaluation/app/swagger-original.yml 	modified:   project-insurance-evaluation/app/templates/home.html

--- project-insurance-evaluation/app/static/js/home.js
+++ project-insurance-evaluation/app/static/js/home.js
@@ -50,12 +50,47 @@ ns.model = (function() {
 ns.view = (function() {
     'use strict';
 
-    /*
-    let $fname = $('#fname'),
-        $lname = $('#lname');
-    */
     // return the API
     return {
+        show_response: function(data) {
+
+            var resposta = JSON.parse(data);
+
+            console.log('resposta.response = '+resposta.response);
+
+            let $response = $("#response");
+
+            $response.removeClass('alert alert-primary');
+            $response.removeClass('alert alert-secondary');
+            $response.removeClass('alert alert-warning');
+            $response.removeClass('alert alert-danger');
+
+            if(resposta.response == 'None') {
+                $response.html('Accident severity level: None.');
+                $response.addClass('alert alert-primary');
+                $response.css('visibility','visible');
+            }
+
+            if(resposta.response == 'Mild') {
+                $response.html('Accident severity level: Mild.');
+                $response.addClass('alert alert-secondary');
+                $response.css('visibility','visible');
+            }
+
+            if(resposta.response == 'Moderate') {
+                $response.html('Accident severity level: Moderate.');
+                $response.addClass('alert alert-warning');
+                $response.css('visibility','visible');
+            }
+
+            if(resposta.response == 'Severe') {
+                $response.html('Accident severity level: Severe.');
+                $response.addClass('alert alert-danger');
+                $response.css('visibility','visible');
+            }
+
+            //alert($response.innerHTML);
+        },
         error: function(error_msg) {
             $('.error')
                 .text(error_msg)
@@ -77,7 +112,8 @@ ns.controller = (function(m, v) {
         $driver_quality = $('#driver_quality'),
         $this_car_cost = $('#this_car_cost'),
         $prop_cost = $('#prop_cost'),
-        $other_car_cost = $('#other_car_cost');
+        $other_car_cost = $('#other_car_cost'),
+        $response = $("#span_response");
 
     // Validate input
     function validate(driver_quality,this_car_cost,prop_cost,other_car_cost) {
@@ -108,7 +144,8 @@ ns.controller = (function(m, v) {
     })
 
     $event_pump.on('model_create_success', function(e, data) {
-        alert('model_create_success!');
+        //alert('model_create_success!');
+        view.show_response(data);
     });
 
     $event_pump.on('model_error', function(e, xhr, textStatus, errorThrown) {
